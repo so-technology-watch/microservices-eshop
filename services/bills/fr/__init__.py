2da5f3d45ab817188c9666d9de3cca9de93844c5
DUNEAU <florianduneaufamily@hotmail.fr>
2017-04-11 11:46:01 +0200

    init du projet, README pour les commandes d'installation

---
+++ services/bills/fr/__init__.py
@@ -0,0 +1,5 @@
+
+
+if "__main__" == __name__:
+    import fr.sogeti.rest as rest
+    rest.run()
---
+++ services/bills/fr/sogeti/rest/__init__.py
@@ -0,0 +1,6 @@
+from fr.sogeti.rest.bills_resource import *
+
+
+def run():
+    from bottle import run
+    run(host='localhost', port=8080)
