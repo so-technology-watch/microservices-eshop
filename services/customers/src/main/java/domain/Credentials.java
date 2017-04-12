4322e3bd29d6845c50a9f41c8405c49f2627b419
LECHELON <lechelon.guillaume@gmail.com>
2017-04-12 11:43:54 +0200

    cleaning in progress

--- services/customers/src/main/java/domain/Credentials.java
+++ services/customers/src/main/java/domain/Credentials.java
@@ -1,31 +1,37 @@
 package domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Credentials {
 
     private String email;
     private String passWord;
 
     public Credentials(String email, String passWord) {
-	
+
 	this.email = email;
 	this.passWord = passWord;
     }
 
+    @JsonProperty
     public String getEmail() {
 
 	return email;
     }
 
+    @JsonProperty
     public void setEmail(String email) {
 
 	this.email = email;
     }
 
+    @JsonProperty
     public String getPassWord() {
 
 	return passWord;
     }
 
+    @JsonProperty
     public void setPassWord(String passWord) {
 
 	this.passWord = passWord;
--- services/customers/src/main/java/domain/Customer.java
+++ services/customers/src/main/java/domain/Customer.java
@@ -4,79 +4,91 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Customer {
 
-	private int id;
-	private String firstname;
-	private String lastname;
-	private String email;
-	private String address;
-	private String phoneNumber;
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private String phoneNumber;
 
-	public Customer(int id, String firstname, String lastname, String email, String address, String phoneNumber) {
-		this.id = id;
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.email = email;
-		this.address = address;
-		this.phoneNumber = phoneNumber;
-	}
+    public Customer(int id, String firstname, String lastname, String email, String address, String phoneNumber) {
+	this.id = id;
+	this.firstname = firstname;
+	this.lastname = lastname;
+	this.email = email;
+	this.address = address;
+	this.phoneNumber = phoneNumber;
+    }
 
-	@JsonProperty
-	public int getId() {
-		return id;
-	}
+    @JsonProperty
+    public int getId() {
 
-	@JsonProperty
-	public void setId(int id) {
-		this.id = id;
-	}
+	return id;
+    }
 
-	@JsonProperty
-	public String getFirstname() {
-		return firstname;
-	}
+    @JsonProperty
+    public void setId(int id) {
 
-	@JsonProperty
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+	this.id = id;
+    }
 
-	@JsonProperty
-	public String getLastname() {
-		return lastname;
-	}
+    @JsonProperty
+    public String getFirstname() {
 
-	@JsonProperty
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+	return firstname;
+    }
 
-	@JsonProperty
-	public String getEmail() {
-		return email;
-	}
+    @JsonProperty
+    public void setFirstname(String firstname) {
 
-	@JsonProperty
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	this.firstname = firstname;
+    }
 
-	@JsonProperty
-	public String getAddress() {
-		return address;
-	}
+    @JsonProperty
+    public String getLastname() {
 
-	@JsonProperty
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	return lastname;
+    }
 
-	@JsonProperty
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    @JsonProperty
+    public void setLastname(String lastname) {
 
-	@JsonProperty
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+	this.lastname = lastname;
+    }
+
+    @JsonProperty
+    public String getEmail() {
+
+	return email;
+    }
+
+    @JsonProperty
+    public void setEmail(String email) {
+
+	this.email = email;
+    }
+
+    @JsonProperty
+    public String getAddress() {
+
+	return address;
+    }
+
+    @JsonProperty
+    public void setAddress(String address) {
+
+	this.address = address;
+    }
+
+    @JsonProperty
+    public String getPhoneNumber() {
+
+	return phoneNumber;
+    }
+
+    @JsonProperty
+    public void setPhoneNumber(String phoneNumber) {
+
+	this.phoneNumber = phoneNumber;
+    }
 }
