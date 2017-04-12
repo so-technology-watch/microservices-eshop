d816ded1fe2ba28febfc4b7e1e04321edcf6fb51
LECHELON <lechelon.guillaume@gmail.com>
2017-04-12 11:30:08 +0200

    Ajout du service Customers

---
+++ services/customers/src/main/java/main/CustomersApplication.java
@@ -0,0 +1,19 @@
+package main;
+
+import config.CustomersConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class CustomersApplication extends Application<CustomersConfiguration>{
+
+	
+
+	@Override
+	public void run(CustomersConfiguration arg0, Environment arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
