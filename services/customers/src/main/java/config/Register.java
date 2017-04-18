ceffe05b543a4eb83c95d4973f1af2bcbd96c511
LECHELON <lechelon.guillaume@gmail.com>
2017-04-18 11:35:33 +0200

    ajout javadoc à register

--- services/customers/src/main/java/config/Register.java
+++ services/customers/src/main/java/config/Register.java
@@ -5,17 +5,35 @@ import java.util.Arrays;
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.agent.model.NewService;
 
+/**
+ * Class dealing with consul registration.
+ * 
+ * @author guilluame
+ *
+ */
 public class Register {
 
+    /**
+     * The consul client
+     */
     private ConsulClient consulClient;
+    /**
+     * The new service
+     */
     private NewService newService;
 
+    /**
+     * 
+     */
     public Register() {
 	this.consulClient = new ConsulClient("10.226.159.191");
 	this.newService = new NewService();
 
     }
 
+    /**
+     * Registers the new service to consul.
+     */
     public void register() {
 
 	newService.setId("customers");
@@ -31,21 +49,38 @@ public class Register {
 
     }
 
+    /**
+     * 
+     * @return consulClient
+     */
     public ConsulClient getConsulClient() {
 
 	return consulClient;
     }
 
+    /**
+     * 
+     * @param consulClient
+     */
     public void setConsulClient(ConsulClient consulClient) {
 
 	this.consulClient = consulClient;
     }
 
+    /**
+     * newClient
+     * 
+     * @return
+     */
     public NewService getNewService() {
 
 	return newService;
     }
 
+    /**
+     * 
+     * @param newService
+     */
     public void setNewService(NewService newService) {
 
 	this.newService = newService;
