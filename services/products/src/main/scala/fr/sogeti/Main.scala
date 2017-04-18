1452cad3955b7ac1a49863bd8b33ba53c2b0549a
DUNEAU <florianduneaufamily@hotmail.fr>
2017-04-18 16:02:56 +0200

    ajout de la récupération de l'host consul sur une variable d'environement, CONSUL_CLIENT

--- services/products/src/main/scala/fr/sogeti/Main.scala
+++ services/products/src/main/scala/fr/sogeti/Main.scala
@@ -5,13 +5,30 @@ import io.vertx.lang.scala.ScalaVerticle
 import fr.sogeti.verticles.{HttpServerVerticle, ClientAmqpVerticle}
 import fr.sogeti.consul.{ServiceDiscovery, ConsulSingleton}
 import io.vertx.core.{Handler, AsyncResult}
+import com.ecwid.consul.v1.agent.model.NewService
+import fr.sogeti.consul.Config
 
 object Main extends App {
+  try{
+    val consulClient = sys.env("CONSUL_CLIENT")
+  }catch{
+    case e : NoSuchElementException => {
+      println("CONSUL_CLIENT environment variable not set properly")
+      sys.exit(1)  
+    }
+  }
   
   val serviceDiscovery : ServiceDiscovery = ConsulSingleton.getServiceDiscoveryInstance
   
+  val config : Config = ConsulSingleton.getConfigResolverInstance.getConfig
+  
+  
   serviceDiscovery.unregister
-  serviceDiscovery.register
+  serviceDiscovery.register(config.getPort)
+  
+  val check : NewService.Check = new NewService.Check()
+  check.setHttp("http://localhost:%d".format(config.getPort))
+  check.setInterval("30s")
   
   val vertx = Vertx.vertx
   
