fe642cd18c2dc426207a4f44c7c62a2f3d809214
LECHELON <lechelon.guillaume@gmail.com>
2017-04-19 10:50:24 +0200

    Ajout de la configuration via consul

---
+++ services/customers/src/main/java/config/KVStore.java
@@ -0,0 +1,31 @@
+package config;
+
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.kv.model.GetValue;
+import com.google.gson.Gson;
+
+public class KVStore {
+
+    private ConsulClient consulClient;
+
+    public KVStore() {
+
+	this.consulClient = new ConsulClient("10.226.159.191");
+    }
+
+    public Configuration retrieveConfig() {
+
+	try {
+	   Response<GetValue> configJSON = consulClient.getKVValue("config/services/customers");
+	    Gson gson = new Gson();
+	    return gson.fromJson(configJSON.getValue().getDecodedValue(), Configuration.class);
+
+	} catch (Exception e) {
+
+	    e.printStackTrace();
+	    return null;
+	}
+    }
+
+}
