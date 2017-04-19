765d3d3a7a8a9046899041fc8248a9fa0354583b
DUNEAU <florianduneaufamily@hotmail.fr>
2017-04-19 14:21:18 +0200

    ajout d'une connexion à la base de données avant le lancement des verticles, pour eviter le timeout de vertx sur la raspberry

--- services/products/src/main/scala/fr/sogeti/Main.scala
+++ services/products/src/main/scala/fr/sogeti/Main.scala
@@ -7,6 +7,11 @@ import fr.sogeti.consul.{ServiceDiscovery, ConsulSingleton}
 import io.vertx.core.{Handler, AsyncResult}
 import com.ecwid.consul.v1.agent.model.NewService
 import fr.sogeti.consul.Config
+import org.slf4j.LoggerFactory
+import fr.sogeti.dao.common.ManagerFactory
+import fr.sogeti.entities.Product
+import fr.sogeti.verticles.ClientAmqpVerticle
+import fr.sogeti.verticles.ClientAmqpVerticle
 
 object Main extends App {
   try{
@@ -18,16 +23,20 @@ object Main extends App {
     }
   }
   
+  if(!initTestDatabase){
+    println("unable to log to the database")
+  }
+  
   val serviceDiscovery : ServiceDiscovery = ConsulSingleton.getServiceDiscoveryInstance
   
   val config : Config = ConsulSingleton.getConfigResolverInstance.getConfig
-  
+  println("found configuration : %s".format(config))
   
   serviceDiscovery.unregister
   serviceDiscovery.register(config.getPort)
   
   val check : NewService.Check = new NewService.Check()
-  check.setHttp("http://localhost:%d".format(config.getPort))
+  check.setHttp("http://%s:%d".format(config.getAddress, config.getPort))
   check.setInterval("30s")
   
   val vertx = Vertx.vertx
@@ -35,4 +44,20 @@ object Main extends App {
   vertx.deployVerticle( ScalaVerticle.nameForVerticle[HttpServerVerticle] )
   vertx.deployVerticle( ScalaVerticle.nameForVerticle[ClientAmqpVerticle] )
   
+  /**
+   * test the connection to the database in order to not be killed by vert.x
+   * raspberry pi takes to long time to init the database and cause a timeout
+   */
+  private def initTestDatabase() : Boolean = {
+    try {
+      val manager = ManagerFactory.getInstance.createEntityManager
+      manager.find(classOf[Product], -1)
+      return true
+    }catch{
+      case e : Exception => {
+        e.printStackTrace
+        return false
+      }
+    }
+  }
 }
