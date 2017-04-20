03754a51082659477f0ca367eecefeac432eaa8d
LECHELON <lechelon.guillaume@gmail.com>
2017-04-20 17:01:10 +0200

    modif scipts

--- dockerfiles/customers/customers.sh
+++ dockerfiles/customers/customers.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-env = '10.226.159.191:8500'
-sudo docker run -d -e $env -p 8082:8082 -it lechelong/customers:latest
+env = 'consul=10.226.159.191:8500'
+sudo docker run -d -e $env -p 8080:8080 -it lechelong/customers:latest
+
+exit 0
