d6e3b7e337e002045811817da19e5ab258915e9d
LECHELON <lechelon.guillaume@gmail.com>
2017-04-20 16:58:58 +0200

    ajout d'un sricpt de lancement pour le service customers

--- dockerfiles/carts/app/carts.sh
+++ dockerfiles/carts/app/carts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 env = 'consul=10.226.159.191:8500'
 sudo docker run -d -e $env -p 8082:8082 -it lechelong/carts:latest
 
--- dockerfiles/customers/customers.sh
+++ dockerfiles/customers/customers.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 env = '10.226.159.191:8500'
 sudo docker run -d -e $env -p 8082:8082 -it lechelong/customers:latest
