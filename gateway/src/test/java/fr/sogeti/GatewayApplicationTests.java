f083ee548ed415ce646c82c44960a03004ddabf2
DUNEAU <florianduneaufamily@hotmail.fr>
2017-04-21 10:04:57 +0200

    init spring cloud

---
+++ gateway/src/test/java/fr/sogeti/GatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.sogeti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
