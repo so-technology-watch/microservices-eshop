32fb0cf79e95deb8ba139f01e37729b3f7108f44
fduneau <florianduneaufamily@hotmail.fr>
2017-04-25 15:26:32 +0200

    ajout des routes en String statiques, correction des methodes

--- fr.sogeti.scenarioTest/src/test/java/fr/sogeti/scenarioTest/ScenarioTest.java
+++ fr.sogeti.scenarioTest/src/test/java/fr/sogeti/scenarioTest/ScenarioTest.java
@@ -1,26 +1,30 @@
 package fr.sogeti.scenarioTest;
 
-import static com.jayway.restassured.RestAssured.*;
-import com.jayway.restassured.http.ContentType;
-
 import beans.Categorie;
 import beans.Client;
 import beans.Facture;
 import beans.Fournisseur;
 import beans.Panier;
 import beans.Produit;
+import static com.jayway.restassured.RestAssured.*;
+import com.jayway.restassured.http.ContentType;
+import static java.lang.String.format;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import static org.hamcrest.Matchers.*;
-
-import java.awt.Panel;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class ScenarioTest extends FonctionalTest {
 
+    private static final String ROUTE_CATEGORY = "/categories";
+    private static final String ROUTE_CLIENT = "/customers";
+    private static final String ROUTE_FACTURE = "/bills";
+    private static final String ROUTE_FOURNISSEUR = "/suppliers";
+    private static final String ROUTE_PANIER = "/carts";
+    private static final String ROUTE_PRODUIT = "/products";
+    
 	public static Object[][] params() {
 
 		return new Object[][] {};
@@ -48,108 +52,72 @@ public class ScenarioTest extends FonctionalTest {
 		verifPanierVide(client);
 
 	}
+    private void creerClient(Client client) {
 
-	private void verifPanierVide(Client client) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void recupFacture(Facture facture) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void ajoutFacture(Panier panier) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void achat(Client client) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void ajoutProduitPanier(Produit produit, Client client) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void verifProduitDansCategorie(Categorie categorie, Produit produit) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void rechercheProduit(Produit produit) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void creationProduit(Produit produit) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void recupProduit(Produit produit) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void recupCategorie(Categorie categorie) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void creationCategorie(Categorie categorie) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void recupFounisseur(Fournisseur fournisseur) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void creationFounisseur(Fournisseur fournisseur) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void creerClient(Client client) {
-
-		String customerJSON = String.format(
+		String customerJSON = format(
 				"{\"id\":%d,\"firstname\":\"%s\",\"lastname\":\"%s\",\"email\":\"%s\","
-						+ "\"credentials\":{\"email\":\"%s\",\"passWord\":\"%s\"},"
+						+ "\"credentials\":{\"email\":\"%s\",\"password\":\"%s\"},"
 						+ "\"address\":\"%s\",\"phoneNumber\":\"%s\"}",
-				id, firstname, lastname, email, email, passsword, address, phoneNumber);
+				client.getId(), client.getFirstname(), client.getLastname(), client.getEmail(), 
+                client.getEmail(), client.getPasssword(), client.getAddress(), client.getPhoneNumber());
 		System.out.println(customerJSON);
 
 		given().contentType(ContentType.JSON).body(customerJSON).when().post("/customers").then()
-				.body(containsString("" + id));
+				.body(containsString("" + client.getId()));
 	}
-
-	public void recupClient(Client client) {
-		String route = "/customers/" + id;
-		get(route).then().assertThat().body("id", equalTo(id));
-		get(route).then().assertThat().body("firstname", equalTo(firstname));
-		get(route).then().assertThat().body("lastname", equalTo(lastname));
-		get(route).then().assertThat().body("email", equalTo(email));
-		get(route).then().assertThat().body("address", equalTo(address));
-		get(route).then().assertThat().body("phoneNumber", equalTo(phoneNumber));
+    
+    public void recupClient(Client client) {
+		String route = ROUTE_CLIENT + "/" + client.getId();
+		get(route).then().assertThat().body("id", equalTo(client.getId()));
+		get(route).then().assertThat().body("firstname", equalTo(client.getFirstname()));
+		get(route).then().assertThat().body("lastname", equalTo(client.getLastname()));
+		get(route).then().assertThat().body("email", equalTo(client.getEmail()));
+		get(route).then().assertThat().body("address", equalTo(client.getAddress()));
+		get(route).then().assertThat().body("phoneNumber", equalTo(client.getPhoneNumber()));
 
 		get(route).then().assertThat().contentType(ContentType.JSON);
 		get(route).then().assertThat().statusCode(200);
 	}
+    
+	private void creationFounisseur(Fournisseur fournisseur) {
+	}
+    
+	private void recupFounisseur(Fournisseur fournisseur) {
+	}
+    
+    
+	private void creationCategorie(Categorie categorie) {
+	}
+    
+    public void recupProduit(Produit produit) {
+	}
+    
+    private void creationProduit(Produit produit) {
+	}
+    
+    private void rechercheProduit(Produit produit) {
+	}
+    
+    
+	private void verifPanierVide(Client client) {
+	}
 
-	public void recupProduit(int id, String reference, String designation, float price, int idSupplier,
-			String companyName, String email, String phoneNumber, String image, int idCategory) {
-		String customerJSON = String.format(
-				"{\"id\":%d,\"reference\":\"%s\",\"designation\":\"%s\",\"price\":%f,"
-						+ "\"supplier\":{\"id\": %d,\"companyName\":\"%s\", \"email\": \"%s\", \"phoneNumber\": \"%s\"},"
-						+ "\"idSupplier\": %d,\"image\":\"%s\", \"idCategory\": %d}",
-				id, reference, designation, price, idSupplier, companyName, email, phoneNumber, idSupplier, image,
-				idCategory);
-
-		given().when().get("/products/" + id).then().body(containsString(customerJSON));
+    private void verifProduitDansCategorie(Categorie categorie, Produit produit) {
+	}
+    
+    private void achat(Client client) {
+	}
+    
+    private void ajoutFacture(Panier panier) {
+	}
+    
+    private void recupFacture(Facture facture) {
+	}
+    
+    private void recupCategorie(Categorie categorie) {
+	}
+    
+	private void ajoutProduitPanier(Produit produit, Client client) {
 	}
 
 	public void clean(Client client, Produit produit, Categorie categorie, Fournisseur fournisseur, Panier panier,
@@ -162,27 +130,17 @@ public class ScenarioTest extends FonctionalTest {
 	}
 
 	private void deleteFacture(Facture facture) {
-		// TODO Auto-generated method stub
-
 	}
 
 	private void deleteClient(Client client) {
-		// TODO Auto-generated method stub
-
 	}
 
 	private void deleteCategorie(Categorie categorie) {
-		// TODO Auto-generated method stub
-
 	}
 
 	private void deleteFournisseur(Fournisseur fournisseur) {
-		// TODO Auto-generated method stub
-
 	}
 
 	private void deleteProduit(Produit produit) {
-		// TODO Auto-generated method stub
-
 	}
 }
