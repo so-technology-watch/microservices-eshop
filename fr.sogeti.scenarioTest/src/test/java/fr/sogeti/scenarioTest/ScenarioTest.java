51265b2948dbbad5975e6964096da6102c2ec707
LECHELON <lechelon.guillaume@gmail.com>
2017-04-25 14:52:11 +0200

    refactor classe de test

--- fr.sogeti.scenarioTest/src/test/java/fr/sogeti/scenarioTest/ScenarioTest.java
+++ fr.sogeti.scenarioTest/src/test/java/fr/sogeti/scenarioTest/ScenarioTest.java
@@ -2,9 +2,19 @@ package fr.sogeti.scenarioTest;
 
 import static com.jayway.restassured.RestAssured.*;
 import com.jayway.restassured.http.ContentType;
+
+import beans.Categorie;
+import beans.Client;
+import beans.Facture;
+import beans.Fournisseur;
+import beans.Panier;
+import beans.Produit;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import static org.hamcrest.Matchers.*;
+
+import java.awt.Panel;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,32 +22,99 @@ import org.junit.runner.RunWith;
 public class ScenarioTest extends FonctionalTest {
 
 	public static Object[][] params() {
-		return new Object[][] {
-				new Object[] { 2, "jean", "paul", "jean.paul2@mail.com", "password", "4 rue de la bergerie",
-						"0956787678" },
-				new Object[] { 3, "paul", "jean", "guillaume@mail.com", "fjeufhufrdk", "5 rue de la pierre ronde",
-						"6060606065" },
-				new Object[] { 2, "guss", "paulin", "jean.paul2@mail.com", "password", "6 rue jean paul",
-						"08979594328" },
-				new Object[] { 2, "guss", "paulin", "jean.paul2@mail.com", "password", "6 rue jean paul",
-						"08979594328" },
-				new Object[] { 2, "same", "mail", "jean.paul2@mail.com", "password", "6 rue jean paul",
-						"08979594328" }, };
+
+		return new Object[][] {};
 	}
 
 	@Test
 	@Parameters(method = "params")
-	public void run(int id, String firstname, String lastname, String email, String password, String address,
-			String phoneNumber) {
+	public void run(Client client, Produit produit, Categorie categorie, Fournisseur fournisseur, Panier panier,
+			Facture facture) {
 
-		creerClient(id, firstname, lastname, email, password, address, phoneNumber);
-		recupClient(id, firstname, lastname, email, address, phoneNumber);
-		//recupProduit(id, reference, designation, price, idSupplier, companyname, email, phoneNumber, image, idCategory);
-		deleteClient(id);
+		creerClient(client);
+		recupClient(client);
+		creationFounisseur(fournisseur);
+		recupFounisseur(fournisseur);
+		creationCategorie(categorie);
+		recupCategorie(categorie);
+		creationProduit(produit);
+		recupProduit(produit);
+		rechercheProduit(produit);
+		verifProduitDansCategorie(categorie, produit);
+		ajoutProduitPanier(produit, client);
+		achat(client);
+		ajoutFacture(panier);
+		recupFacture(facture);
+		verifPanierVide(client);
+
 	}
 
-	private void creerClient(int id, String firstname, String lastname, String email, String passsword, String address,
-			String phoneNumber) {
+	private void verifPanierVide(Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void recupFacture(Facture facture) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ajoutFacture(Panier panier) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void achat(Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ajoutProduitPanier(Produit produit, Client client) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void verifProduitDansCategorie(Categorie categorie, Produit produit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void rechercheProduit(Produit produit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void creationProduit(Produit produit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void recupProduit(Produit produit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void recupCategorie(Categorie categorie) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void creationCategorie(Categorie categorie) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void recupFounisseur(Fournisseur fournisseur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void creationFounisseur(Fournisseur fournisseur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void creerClient(Client client) {
 
 		String customerJSON = String.format(
 				"{\"id\":%d,\"firstname\":\"%s\",\"lastname\":\"%s\",\"email\":\"%s\","
@@ -50,8 +127,7 @@ public class ScenarioTest extends FonctionalTest {
 				.body(containsString("" + id));
 	}
 
-	public void recupClient(int id, String firstname, String lastname, String email, String address,
-			String phoneNumber) {
+	public void recupClient(Client client) {
 		String route = "/customers/" + id;
 		get(route).then().assertThat().body("id", equalTo(id));
 		get(route).then().assertThat().body("firstname", equalTo(firstname));
@@ -64,13 +140,10 @@ public class ScenarioTest extends FonctionalTest {
 		get(route).then().assertThat().statusCode(200);
 	}
 
-	public void creerProduit() {
-	}
-
-	public void recupProduit(int id, String reference, String designation, int price, int idSupplier,
-			String companyName, String email, String phoneNumber, String image, String idCategory) {
+	public void recupProduit(int id, String reference, String designation, float price, int idSupplier,
+			String companyName, String email, String phoneNumber, String image, int idCategory) {
 		String customerJSON = String.format(
-				"{\"id\":%d,\"reference\":\"%s\",\"designation\":\"%s\",\"price\":%d,"
+				"{\"id\":%d,\"reference\":\"%s\",\"designation\":\"%s\",\"price\":%f,"
 						+ "\"supplier\":{\"id\": %d,\"companyName\":\"%s\", \"email\": \"%s\", \"phoneNumber\": \"%s\"},"
 						+ "\"idSupplier\": %d,\"image\":\"%s\", \"idCategory\": %d}",
 				id, reference, designation, price, idSupplier, companyName, email, phoneNumber, idSupplier, image,
@@ -79,28 +152,37 @@ public class ScenarioTest extends FonctionalTest {
 		given().when().get("/products/" + id).then().body(containsString(customerJSON));
 	}
 
-	public void rechercheProduit() {
+	public void clean(Client client, Produit produit, Categorie categorie, Fournisseur fournisseur, Panier panier,
+			Facture facture) {
+		deleteClient(client);
+		deleteProduit(produit);
+		deleteFournisseur(fournisseur);
+		deleteCategorie(categorie);
+		deleteFacture(facture);
+	}
+
+	private void deleteFacture(Facture facture) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void clientAjouteProduitPanier() {
+	private void deleteClient(Client client) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void clientConsultePanier() {
+	private void deleteCategorie(Categorie categorie) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void clientAchete() {
+	private void deleteFournisseur(Fournisseur fournisseur) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void consulterFacture() {
+	private void deleteProduit(Produit produit) {
+		// TODO Auto-generated method stub
 
 	}
-
-	public void deleteClient(int id) {
-		delete("/customers/" + id).then().assertThat().statusCode(200);
-	}
-
 }
