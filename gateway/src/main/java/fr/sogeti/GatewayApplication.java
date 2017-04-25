72ad2c2ec409cf6590447c70401482e8caedd650
LECHELON <lechelon.guillaume@gmail.com>
2017-04-25 10:00:26 +0200

    ajout d'un projet de scenario de test

--- gateway/src/main/java/fr/sogeti/GatewayApplication.java
+++ gateway/src/main/java/fr/sogeti/GatewayApplication.java
@@ -4,9 +4,12 @@ package fr.sogeti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableHystrixDashboard
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
