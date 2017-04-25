8033a6563c0d90957bce2b9545040aae6d07bf98
LECHELON <lechelon.guillaume@gmail.com>
2017-04-25 10:19:06 +0200

    ajout de junit

--- fr.sogeti.scenarioTest/src/test/java/fr/sogeti/scenarioTest/ScenarioTest.java
+++ fr.sogeti.scenarioTest/src/test/java/fr/sogeti/scenarioTest/ScenarioTest.java
@@ -1,14 +1,17 @@
 package fr.sogeti.scenarioTest;
 
 import static org.junit.Assert.*;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import org.junit.Test;
 
 public class ScenarioTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void creerClient() {
+		
+		String client = "{\"CustomerID\": 1, [{\"ElementID\":2,\"ProductID\":1,\"Quantity\":1,\"UnitPrice\":1}]}";
 	}
 
 }
