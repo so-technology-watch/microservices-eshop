4bc3cb5b9224c916c50e93d41f678f2e72d3e52e
fduneau <florianduneaufamily@hotmail.fr>
2017-04-25 15:08:43 +0200

    implémentation contenu des beans de tests

--- fr.sogeti.scenarioTest/src/test/java/beans/Client.java
+++ fr.sogeti.scenarioTest/src/test/java/beans/Client.java
@@ -1,5 +1,78 @@
 package beans;
 
 public class Client {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String passsword;
+    private String address;
+    private String phoneNumber;
 
+    public Client(int id, String firstname, String lastname, String email, String passsword, String address, String phoneNumber) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.passsword = passsword;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
 }
--- fr.sogeti.scenarioTest/src/test/java/beans/Produit.java
+++ fr.sogeti.scenarioTest/src/test/java/beans/Produit.java
@@ -1,8 +1,78 @@
 package beans;
 
 public class Produit {
-	
-	
-	
+	private int id;
+    private String reference;
+    private String designation;
+    private String image;
+    private double price;
+    private int idSupplier;
+    private int idCategory;
 
+    public Produit(int id, String reference, String designation, String image, double price, int idSupplier, int idCategory) {
+        this.id = id;
+        this.reference = reference;
+        this.designation = designation;
+        this.image = image;
+        this.price = price;
+        this.idSupplier = idSupplier;
+        this.idCategory = idCategory;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getIdSupplier() {
+        return idSupplier;
+    }
+
+    public void setIdSupplier(int idSupplier) {
+        this.idSupplier = idSupplier;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+    
 }
