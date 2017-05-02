dd47cfe6d14cd61d32e663f87455d66ec2c20c54
fduneau <florianduneaufamily@hotmail.fr>
2017-05-02 11:38:43 +0200

    ajout de la configuration de l'adresse de consul en dynamique, début ajout de l'authentification

--- gateway/src/main/java/fr/sogeti/GatewayApplication.java
+++ gateway/src/main/java/fr/sogeti/GatewayApplication.java
@@ -1,22 +1,47 @@
 
 package fr.sogeti;
 
+import static java.lang.String.format;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+//@EnableOAuth2Resource
+@EnableOAuth2Sso
+@EnableOAuth2Client
 @RestController
+@Service
 public class GatewayApplication {
     
 	public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        String consulAddress = System.getenv("CONSUL_CLIENT");
+        if(Objects.isNull(consulAddress)){
+            System.err.println("Please set the CONSUL_CLIENT environment variable properly");
+            return;
+        }
+        
+        List<String> arguments = new ArrayList<>(Arrays.asList(args));
+        String consulHost = "--spring.cloud.consul.host";
+        
+        if(arguments.stream().noneMatch(s -> s.contains(consulHost))){
+            arguments.add(format("%s=%s", consulHost, consulAddress));
+        }
+        
+        args = arguments.toArray(new String[arguments.size()]);
+        ConfigurableApplicationContext spring = SpringApplication.run(GatewayApplication.class, args);
 	}
 }
