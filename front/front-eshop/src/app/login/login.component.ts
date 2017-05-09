17f540e5e53205961392f8f08ecb8e02824adf0f
LECHELON <lechelon.guillaume@gmail.com>
2017-05-09 12:08:46 +0200

    split comonent login

--- front/front-eshop/src/app/login/login.component.ts
+++ front/front-eshop/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Headers, RequestOptions, Http } from '@angular/http';
+import { Credentials } from './credentials';
+import { AuthResponse } from './authResponse';
+import { LoginService } from './login.service';
 
 
 @Component({
@@ -10,40 +13,21 @@ export class LoginComponent {
 
   constructor(private http: Http) { }
 
+
   submitted = false
   credentials = new Credentials("mail2@mail.fr", "passijjfeij");
   authResponse = new AuthResponse();
+  loginService = new LoginService(this.http);
 
   auth() {
-    let body = JSON.stringify(this.credentials)
-    let url = 'http://10.226.160.79:9090/api/v1/auth';
-    let headers = new Headers({ 'content-type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
 
-    this.http.post(url, body, options).subscribe(
-      response => {
-        this.authResponse.code = response.json().code;
-        this.authResponse.content = response.json().content;
-      },
-      err => { console.log() });
+    this.authResponse = this.loginService.authenticate(this.credentials);
+
+    if (this.authResponse != null ){
+      
+    }
   }
-
   get json() { return JSON.stringify(this.credentials); }
   get authResponseJSON() { return JSON.stringify(this.authResponse); }
-
-}
-
-class Credentials {
-
-  constructor(
-    public email: String,
-    public password: String
-  ) { }
-}
-
-class AuthResponse {
-
-  code: Number;
-  content: String;
 
 }
