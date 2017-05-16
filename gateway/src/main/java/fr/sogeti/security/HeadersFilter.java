fc42286e0bb0dc63c7d6b4d88aba473a0a40dd82
fduneau <florianduneaufamily@hotmail.fr>
2017-05-16 14:08:55 +0200

    ajout headers au niveau du gateway : 'allaow-method' et 'cros-origin'

--- gateway/src/main/java/fr/sogeti/security/HeadersFilter.java
+++ gateway/src/main/java/fr/sogeti/security/HeadersFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -23,10 +24,26 @@ public class HeadersFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain fc) throws IOException, ServletException {
         if(servletResponse instanceof HttpServletResponse){
             HttpServletResponse response = (HttpServletResponse) servletResponse;
-            response.addHeader("Access-Control-Allow-Origin", "*");
-            response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            addHeader(response, "Access-Control-Allow-Origin", "*");
+            addHeader(response, "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            addHeader(response, "Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+            if(servletRequest instanceof HttpServletRequest){
+                String method = ((HttpServletRequest) servletRequest).getMethod();
+                if(method.equals("OPTIONS")){
+                    response.getOutputStream().println("OK");
+                    return;
+                }
+            }
         }
         fc.doFilter(servletRequest, servletResponse);
+    }
+    
+    private void addHeader(HttpServletResponse response, String header, String value){
+        if(!response.containsHeader(header)){
+            response.addHeader(header, value);
+        }else{
+            response.setHeader(header, value);
+        }
     }
 
     @Override
