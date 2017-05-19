4a76d14862c97062847be537331d4cc73029268e
LECHELON <lechelon.guillaume@gmail.com>
2017-05-19 15:33:04 +0200

    playing with templates

--- front/front-suppliers/app.py
+++ front/front-suppliers/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "I'm just a supplier front, get over it"
+    return render_template("authPage.html")
 
+##Authentification by email
+@app.route("/auth")
+def auth():
+    return "auth route"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
