ecdb31c3dda4c8b1317f86b1c0c25d3c10f109d1
fduneau <florianduneaufamily@hotmail.fr>
2017-05-22 11:46:44 +0200

    ajout de l'implémentation de la route d'update product et des templates associés

--- front/front-suppliers/app.py
+++ front/front-suppliers/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from services.products_service import ProductService, Product
 
 app = Flask(__name__)
+gateway_url = '10.226.159.191:9090'
+products_service = ProductService(gateway_url)
 
 @app.route("/")
 def root():
@@ -10,6 +13,20 @@ def root():
 @app.route("/auth")
 def auth():
     return "auth route"
+
+@app.route("/products")
+def products_list():
+    id = 141
+    products = products_service.retrieve_products(id)
+    categories = products_service.retrieve_categories()
+    return render_template("products/products.html", products=products, categories=categories)
+
+@app.route("/updateProduct/<id>", methods=['GET','POST'])
+def update_product(id):
+    if request.method == 'POST':
+        product = Product(id, id_supplier=141, **request.form)
+        products_service.update_product(product)
+    return render_template("products/success.html", previous_page="/products")
 
 if __name__ == "__main__":
     app.run(debug = True)
